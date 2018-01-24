a1f979f7e3bf4f1e9dfc8e0fb1cb54372911bfd1
roringbbk <bbk.programmerz@gmail.com>
2018-01-24 23:56:35 +0545

    Add 2 precision for credit and debit

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,17 @@
 class Account < ApplicationRecord
+  include ActionView::Helpers::NumberHelper
+  
   validates :code, presence: true
   validates :name, presence: true
   validates :credit, presence: true
   validates :debit, presence: true
   validates :description, presence: true
+
+  def debit_precision
+    number_with_precision(self.debit, precision: 2)
+  end
+
+  def credit_precision
+    number_with_precision(self.credit, precision: 2)
+  end
 end
